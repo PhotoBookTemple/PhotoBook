9666368cdf0ee5836f2ff66c9806446954f38757
sushma-ananda <tuf77221@temple.edu>
2014-12-03 04:03:52 -0500

    Bug fixes - backkey press - close parent activity, suppress keyboard Implemented Notification service on upload

---
+++ checkDBConn.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$database = "Fa14_4340_Photobook";
+$username = "tuf77221";
+$password = "nichohma";
+
+// Create connection
+$mysqlconn = mysqli_connect($servername, $username, $password, $database);
+
+?>
